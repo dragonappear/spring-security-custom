e5e8ae28c428c530b976a12419fd6848141e890a
dragonappear <dragonappear1@gmail.com>
2022-01-24 20:52:15 +0900

    Initial commit

---
+++ src/test/java/me/dragonappear/springsecuritycustom/SpringSecurityCustomApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dragonappear.springsecuritycustom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCustomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
