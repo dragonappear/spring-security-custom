2aaefef35143a484ad7d8a05b0c4f2e8d6a1ea30
dragonappear <dragonappear1@gmail.com>
2022-01-26 16:29:27 +0900

    Resource static 설정 구현

--- src/main/java/me/dragonappear/springsecuritycustom/security/web/config/WebSecurityConfig.java
+++ src/main/java/me/dragonappear/springsecuritycustom/security/web/config/WebSecurityConfig.java
@@ -42,6 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
